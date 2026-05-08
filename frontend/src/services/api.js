4c7449e3ebebac4adce2307e44b5325a7f27f890
Sreenand76 <sreenands93@gmail.com>
2026-05-08 13:51:10 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
+
 // Create axios instance with default config
 const api = axios.create({
   baseURL: API_BASE_URL,
